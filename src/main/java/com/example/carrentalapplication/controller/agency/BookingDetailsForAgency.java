9b935e24b43dd77d69cb5b2b72447b5d1a0a2f0e
Nikita <nrw@narola.email>
2023-07-11 17:56:54 +0530

    Add admin can view their booking details

---
+++ src/main/java/com/example/carrentalapplication/controller/agency/BookingDetailsForAgency.java
@@ -0,0 +1,41 @@
+package com.example.carrentalapplication.controller.agency;
+
+import com.example.carrentalapplication.dao.AgencyDAO;
+import com.example.carrentalapplication.dao.BookDao;
+import com.example.carrentalapplication.exception.DAOException;
+import com.example.carrentalapplication.model.AgencyDetails;
+import com.example.carrentalapplication.model.Book;
+import com.example.carrentalapplication.model.User;
+import com.sun.prism.shader.AlphaOne_LinearGradient_AlphaTest_Loader;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+public class BookingDetailsForAgency extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("CurrentUser");
+        try {
+        AgencyDAO agencyDAO=new AgencyDAO();
+        AgencyDetails agencyDetails=agencyDAO.getAgenciesByUserId(user.getUserId());
+
+        BookDao bookDao=new BookDao();
+
+           List<Book> bookList= bookDao.BookingDataForAgencyAdmin(agencyDetails.getAgencyDetailsId());
+           req.setAttribute("bookingDetails",bookList);
+
+            RequestDispatcher requestDispatcher= req.getRequestDispatcher("AdminViewBooking.jsp");
+            requestDispatcher.forward(req,resp);
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
+    }
+}
