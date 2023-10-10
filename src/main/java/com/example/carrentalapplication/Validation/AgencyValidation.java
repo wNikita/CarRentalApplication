e264b138a4b9f731869bee9b2662ff480a2c6300
Nikita <nrw@narola.email>
2023-10-10 11:46:53 +0530

    Implementing JPA query

--- src/main/java/com/example/carrentalapplication/Validation/AgencyValidation.java
+++ src/main/java/com/example/carrentalapplication/Validation/AgencyValidation.java
@@ -10,7 +10,7 @@ public class AgencyValidation {
     public static List<Error> AgencyValidate(AgencyDetailsDTO agencyDetailsDTO) {
         List<Error> errorList = new ArrayList<>();
         if (Validation.isEmpty(agencyDetailsDTO.getAgencyName())) {
-            errorList.add(new Error("Please enter agencyName"));
+            errorList.add(new Error("Please enter agency name"));
         }
         if (Validation.isEmpty(agencyDetailsDTO.getMobileNumber())) {
             errorList.add(new Error("please enter mobile number"));
@@ -20,7 +20,7 @@ public class AgencyValidation {
         }
         if(Validation.isEmpty(agencyDetailsDTO.getAddressDetailsDTO().getPinCode()))
         {
-            errorList.add(new Error("please enter pincode"));
+            errorList.add(new Error("please enter pin code"));
         }
         return errorList;
     }
