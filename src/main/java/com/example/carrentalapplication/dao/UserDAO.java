229589a520233cbb39c271abe9470a54501e98b5
Nikita <nrw@narola.email>
2024-01-03 17:19:32 +0530

    Updates

--- src/main/java/com/example/carrentalapplication/dao/UserDAO.java
+++ src/main/java/com/example/carrentalapplication/dao/UserDAO.java
@@ -25,6 +25,7 @@ public class UserDAO {
             em.getTransaction().begin();
             em.persist(userEntity);
             em.getTransaction().commit();
+            return userEntity;
         } catch (Exception ex) {
             throw new DAOException("Exception while adding user", ex);
         }
